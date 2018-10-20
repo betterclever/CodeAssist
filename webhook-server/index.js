0fb2312de5e505171df4487cd724682637d81f29
Pranjal Paliwal <paliwal.pranjal83@gmail.com>
2018-10-20 23:24:37 +0530

    Added Webhook server

---
+++ webhook-server/index.js
@@ -0,0 +1,46 @@
+import express from 'express';
+import rethinkdb from 'rethinkdb';
+
+
+const app = express();
+const port = 3000;
+
+function getIntent(intentDN, parameters) {
+    switch (intentDN) {
+        case 'Open Folder': {
+            const type = parameters.folder;
+            const name = parameters.foldername;
+
+            let intent = {
+                'type': 'open',
+                'target': type,
+                'name': name
+            };
+
+            return intent;
+        }
+    }
+
+    return null;
+}
+
+app.post('/incomingIntents', async (req, res) => {
+    let body = req.body;
+
+    let conn = await r.connect({
+        host: "149.28.137.113",
+        port: 28015,
+        db: "CodeAssist"
+    })
+
+    console.log('conn', conn);
+    const parameters = body.queryResult.parameters;
+    const intentDN = body.intent.displayName;
+
+    let i = getIntent(intentDN, parameters);
+    i.id = 'betterclever';
+
+    r.table('UserCommands').update(conn, i);
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
